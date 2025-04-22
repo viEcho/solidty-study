7cb81e35cd54aa61ec69148d276c7ffba471b2ea
viEcho <baoj0719@163.com>
2025-04-23 00:24:38 +0800

    C3算法确保每个父合约的函数按继承链顺序只被调用一次

---
+++ 3w/033ParentFunctions.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.3;
+
+contract E{
+    event Log(string message);
+
+    function foo() public virtual {
+        emit Log("E.foo");
+    }
+    function bar() public virtual {
+        emit Log("E.bar");
+    }
+
+}
+
+contract F is E{
+    function foo() public virtual override {
+        emit Log("F.foo");
+        E.foo();
+    }
+    
+    function bar() public virtual override {
+        emit Log("F.bar");
+        super.bar();
+    }
+}
+contract G is E{
+    function foo() public virtual override {
+        emit Log("G.foo");
+        E.foo();
+    }
+    
+    function bar() public virtual override {
+        emit Log("G.bar");
+        super.bar();
+    }
+}
+contract H is G,F{
+    function foo() public  override(G,F) {
+        F.foo();
+    }
+    function bar() public  override(G,F) {
+        super.bar();
+    }
+}
