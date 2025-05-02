1d6506eb6af599daa103d7d189755b3ada618590
viEcho <baoj0719@163.com>
2025-05-02 18:07:09 +0800

    dsdsd

--- 4w/046Hash.sol
+++ 4w/046Hash.sol
@@ -1,12 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-contract HashFunc{
-    function hash(string memory text, uint num,address addr) external pure returns(bytes32){
-        return keccak256(abi.encodePacked(text,num,addr));
+contract HashFunc {
+    function hash(
+        string memory text,
+        uint256 num,
+        address addr
+    ) external pure returns (bytes32) {
+        // abi.encodePacked返回的是一个压缩的bytes
+        return keccak256(abi.encodePacked(text, num, addr));
     }
 
-    
+    function encode(string memory text0, string memory text1)
+        external
+        pure
+        returns (bytes memory)
+    {
+        // 返回是一个完整的bytes
+        return abi.encode(text0, text1);
+    }
 
+     // 
+    function encodePacked(string memory text0, string memory text1)
+        external
+        pure
+        returns (bytes memory)
+    {
+        return abi.encode(text0, text1);
+    }
 
+    function collision(
+        string memory text0,
+        uint256 x,
+        string memory text1
+    ) external pure returns (bytes32) {
+        return keccak256(abi.encodePacked(text0, x, text1));
+    }
 }
